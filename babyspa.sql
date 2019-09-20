d3ab9b92bb6c163fab7e9383796f154300d8d0c1
Yasmini19 <anandayasmin48@gmail.com>
2019-09-20 19:10:16 +0700

    gambar Subkategori dan database

--- babyspa.sql
+++ babyspa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Sep 2019 pada 05.27
+-- Waktu pembuatan: 20 Sep 2019 pada 14.07
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -53,7 +53,7 @@ CREATE TABLE `contact_us` (
 INSERT INTO `contact_us` (`id_contact`, `contact`, `keterangan`) VALUES
 (1, 'Location', 'Malang'),
 (2, 'Phone', '081209329023'),
-(3, 'Social Media', 'instagram : maminaa'),
+(3, 'Social Media', 'instagram : maminaa_'),
 (4, 'Email', 'ertyuio@gmail.com');
 
 -- --------------------------------------------------------
@@ -92,6 +92,15 @@ CREATE TABLE `kategori` (
   `keterangan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `keterangan`) VALUES
+(1, 'Baby'),
+(2, 'Mom'),
+(3, 'Konselor');
+
 -- --------------------------------------------------------
 
 --
@@ -120,13 +129,14 @@ INSERT INTO `level` (`id_level`, `keterangan`) VALUES
 
 CREATE TABLE `reservasi` (
   `id_reservasi` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `pemesan_id` int(11) NOT NULL,
+  `terapis_id` int(11) NOT NULL,
   `sesi_id` int(11) NOT NULL,
   `tgl_reservasi` date NOT NULL,
   `total_harga_awal` int(11) NOT NULL,
   `diskon_pesen` varchar(5) NOT NULL,
   `nominal_diskon` float NOT NULL,
-  `total_harga_akhir` int(11) NOT NULL
+  `biaya_transportasi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -159,10 +169,23 @@ INSERT INTO `sesi_reservasi` (`id_sesi`, `sesi`, `waktu`) VALUES
 
 CREATE TABLE `sub_kategori` (
   `id_sub_kategori` int(11) NOT NULL,
-  `kategori` int(11) NOT NULL,
-  `keterangan` varchar(100) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `judul_sub` varchar(500) NOT NULL,
+  `keterangan` text NOT NULL,
+  `foto_sub` varchar(500) NOT NULL,
   `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `sub_kategori`
+--
+
+INSERT INTO `sub_kategori` (`id_sub_kategori`, `kategori_id`, `judul_sub`, `keterangan`, `foto_sub`, `harga`) VALUES
+(1, 1, 'Massage', 'Pijatan lembut oleh tenaga profesional yang akan membantu memperlancar peredaran darah si kecil. Min', 'baby.jpg', 85000),
+(2, 1, 'Scrub', 'Dalam baby body scrub ini anak akan diberikan bergabai macam body scrub pada bagian tubuhnya yang tentunya terbuat dari bahan – bahan yang aman bagi kulit anak. Seperti lulur lumpur dan cokelat. Baby body scrub ini akan memakan waktu 15 – 20 menit agar scrub tersebut meresap ke dalam kulit anak yang akan memberikan manfaat yang baik.', 'babyscrub.jpg', 90000),
+(3, 1, 'gymball', 'Menguatkan otot-otot dan persendian,Meningkatkan perkembangan motorik,Meningkatkan fleksibilitas atau daya kelenturan tubuh', 'gymball.jpg', 80000),
+(4, 1, 'Swim', 'Begitu masuk kedalam klinik “Baby Spa”, Anda akan langsung disambut oleh 2 kolam besar berwarna putih lengkap dengan bola-bola mainannya. Ini yang menjadi ciri unik Baby spa dibanding tempat spa lainnya. Konsep semi outdoor memudahkan bunda dan keluarga untuk melihat langsung si kecil saat berenang. Baby Swim ini hanya diperuntukkan bagi bayi yang sudah memiliki berat badan minimal 5 kilogram. ', 'b6.jpg', 85000),
+(5, 2, 'yoga ibu hamil', 'Dengan melakukan penetralan yoga, akan mendukung perubahan tubuh yang terjadi dan memperkuat otot tubuh menjelang melahirkan', 'yoga.jpg', 120000);
 
 -- --------------------------------------------------------
 
@@ -177,18 +200,20 @@ CREATE TABLE `user` (
   `email` varchar(100) NOT NULL,
   `no_telp` varchar(14) NOT NULL,
   `alamat` varchar(300) NOT NULL,
+  `foto` varchar(500) DEFAULT NULL,
   `password` varchar(200) NOT NULL,
-  `level` int(11) NOT NULL,
-  `foto` varchar(255) NOT NULL
+  `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id_user`, `full_name`, `username`, `email`, `no_telp`, `alamat`, `password`, `level`, `foto`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com', '091322332234', 'malang', '21232f297a57a5a743894a0e4a801fc3', 1, ''),
-(2, 'Tyson Amery', 'Tyson', 'tysonAmery@gmail.com', '087678987564', 'Malang', 'tyson', 3, 't1.jpg');
+INSERT INTO `user` (`id_user`, `full_name`, `username`, `email`, `no_telp`, `alamat`, `foto`, `password`, `level`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', '091322332234', 'malang', '', '21232f297a57a5a743894a0e4a801fc3', 1),
+(2, 'sasa', 'sasa', 'sasa@123', '0813999293433', 'Malang', '', 'f45731e3d39a1b2330bbf93e9b3de59e', 2),
+(5, 'meli', 'meli', 'meli@gmail.com', '2', 'meli', NULL, '315fef7b8d30f99d6964f489ee4c9828', 2),
+(6, 'yasmini', 'yasmini', 'yasmini@gmail.com', '9029109012', 'malang', 't1.jpg', '0bad1b8571cd7ad70fb02939a1f9b6fb', 3);
 
 --
 -- Indexes for dumped tables
@@ -238,7 +263,7 @@ ALTER TABLE `level`
 ALTER TABLE `reservasi`
   ADD PRIMARY KEY (`id_reservasi`),
   ADD KEY `fk_reservasi_sesi` (`sesi_id`),
-  ADD KEY `fk_reservasi_user` (`user_id`);
+  ADD KEY `fk_reservasi_user` (`pemesan_id`);
 
 --
 -- Indeks untuk tabel `sesi_reservasi`
@@ -251,7 +276,7 @@ ALTER TABLE `sesi_reservasi`
 --
 ALTER TABLE `sub_kategori`
   ADD PRIMARY KEY (`id_sub_kategori`),
-  ADD KEY `fk_subkategori_kategori` (`kategori`);
+  ADD KEY `fk_subkategori_kategori` (`kategori_id`);
 
 --
 -- Indeks untuk tabel `user`
@@ -292,7 +317,7 @@ ALTER TABLE `galery`
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `level`
@@ -316,13 +341,13 @@ ALTER TABLE `sesi_reservasi`
 -- AUTO_INCREMENT untuk tabel `sub_kategori`
 --
 ALTER TABLE `sub_kategori`
-  MODIFY `id_sub_kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_sub_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -340,13 +365,13 @@ ALTER TABLE `detail_reservasi`
 --
 ALTER TABLE `reservasi`
   ADD CONSTRAINT `fk_reservasi_sesi` FOREIGN KEY (`sesi_id`) REFERENCES `sesi_reservasi` (`id_sesi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_reservasi_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_reservasi_user` FOREIGN KEY (`pemesan_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `sub_kategori`
 --
 ALTER TABLE `sub_kategori`
-  ADD CONSTRAINT `fk_subkategori_kategori` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_subkategori_kategori` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `user`
