c675e039a5902c093b45ebc03eb6d8dc9f8d41d0
rmillenia <rmillenia96@yahoo.com>
2019-09-22 18:42:00 +0700

    front end home user, price

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,11 +18,16 @@
             $where2 = array('contact' => 'Phone' );
             $where3 = array('contact' => 'Email' );
             $where4 = array('contact' => 'Social Media');
+
+            $on = 'sub_kategori.kategori_id = kategori.id_kategori';
+
             $data['trps'] = $this->GeneralModel->get_selected('user',$where)->result();
             $data['loc'] = $this->GeneralModel->get_selected('contact_us',$where1)->result();
             $data['phone'] = $this->GeneralModel->get_selected('contact_us',$where2)->result();
             $data['email'] = $this->GeneralModel->get_selected('contact_us',$where3)->result();
             $data['socmed'] = $this->GeneralModel->get_selected('contact_us',$where4)->result();
+            $data['subkategori'] = $this->GeneralModel->get_join('sub_kategori','kategori',$on)->result();
+
             $this->load->view('user/headerfooter/header');
             $this->load->view('user/HomeUser',$data);
             $this->load->view('user/headerfooter/footer');        
