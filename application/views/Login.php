9600d9bd0bcdde03a0a82bc8c352f6804b2a4c85
rmillenia <rmillenia96@yahoo.com>
2019-10-09 22:26:50 +0700

    add reservation

--- application/views/Login.php
+++ application/views/Login.php
@@ -79,7 +79,7 @@
   <script src="<?php echo base_url();?>assets/admin/base/vendor.bundle.base.js"></script>
   <!-- endinject -->
   <!-- inject:js -->
-  <script src="<?php echo base_url();?>assets/admin/js/template.js"></script>
+  <!-- <script src="<?php echo base_url();?>assets/admin/js/template.js"></script> -->
   <!-- endinject -->
 </body>
 
